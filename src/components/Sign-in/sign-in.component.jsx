4bfdb31a39a45202cc9ace8631b9489ceb9eee2c
KamikaziD <druhfus@gmail.com>
2021-02-06 14:11:20 +0200

    updated our fetch locations

--- src/components/Sign-in/sign-in.component.jsx
+++ src/components/Sign-in/sign-in.component.jsx
@@ -18,7 +18,7 @@ class Signin extends React.Component {
     }
 
     onSubmitSignin = () => {
-        fetch('http://localhost:3000/signin', {
+        fetch('https://afternoon-retreat-57509.herokuapp.com/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
